85e2f59234bbaae52cd1544a1d9e3a0dd9c2c1ea
aziis98 <antonio.delucreziis@gmail.com>
2016-09-25 00:48:01 +0200

    A commit

--- src/com/aziis98/ktodo/KTodo.kt
+++ src/com/aziis98/ktodo/KTodo.kt
@@ -1,5 +1,6 @@
 package com.aziis98.ktodo
 
+import com.aziis98.control.Control
 import com.aziis98.kappa.KappaApplication
 import java.awt.Graphics2D
 import java.nio.file.Paths
@@ -8,11 +9,14 @@ import java.nio.file.Paths
  * Created by aziis98 on 22/09/2016.
  */
 object KTodo : KappaApplication() {
-    override fun setup() {
+    val control = Control("test")
 
+    override fun setup() {
+        control.style["width"] = 500
+        control.style["height"] = 750
     }
 
     override fun draw(g: Graphics2D) {
-
+        control.render(g)
     }
 }
