e309175046591fdaf84334fedcdabb7040706c44
aziis98 <antonio.delucreziis@gmail.com>
2016-09-26 20:56:46 +0200

    It's a red rectangle when you hover it!

--- src/com/aziis98/ktodo/KTodo.kt
+++ src/com/aziis98/ktodo/KTodo.kt
@@ -1,22 +1,41 @@
 package com.aziis98.ktodo
 
-import com.aziis98.control.Control
+import com.aziis98.control.ChildControl
+import com.aziis98.control.appendReflective
+import com.aziis98.control.dynamic
 import com.aziis98.kappa.KappaApplication
+import com.aziis98.utils.choose
+import java.awt.Color
+import java.awt.Cursor
 import java.awt.Graphics2D
-import java.nio.file.Paths
 
 /**
  * Created by aziis98 on 22/09/2016.
  */
 object KTodo : KappaApplication() {
-    val control = Control("test")
 
     override fun setup() {
-        control.style["width"] = 500
-        control.style["height"] = 750
+        window.apply {
+            rootContainer.appendReflective<ChildControl>("test1").apply {
+                dynamic {
+                    x = 100
+                    y = 100
+                    width = 150
+                    height = 300
+
+                    backgroundColor =
+                            hover.choose(Color.RED, Color.WHITE)
+                    borderRadius =
+                            hover.choose(5, 0)
+
+                    if (hover) handle.mouse.cursor = Cursor.HAND_CURSOR
+                }
+            }
+
+        }
     }
 
     override fun draw(g: Graphics2D) {
-        control.render(g)
+        window.render(g)
     }
 }
